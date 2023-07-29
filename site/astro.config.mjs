4b9bf59ce381605bb4ad91d924925da5a2b118af
ksenginew <ksengine.github@gmail.com>
2023-07-29 22:38:46 +0530

    commit

---
+++ site/astro.config.mjs
@@ -0,0 +1,32 @@
+import { defineConfig, sharpImageService } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Styless',
+			social: {
+				github: 'https://github.com/withastro/starlight',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', link: '/guides/example/' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+	experimental: {
+		assets: true,
+	},
+	// Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
+	image: { service: sharpImageService() },
+});
